b770a8e689d07feecaaa4c27f4e2674d812f199a
kishanera <kishanera@gmail.com>
2023-05-25 04:05:04 +0530

    commit

---
+++ bussch/lib/main.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bus.sch App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  void login(BuildContext context) {
+    String username = usernameController.text;
+    String password = passwordController.text;
+
+    if (username == '9876543210' && password == 'pass1234') {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => TrackingPage()),
+      );
+    } else {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Login Failed'),
+            content: Text('Invalid username or password. Please try again.'),
+            actions: <Widget>[
+              TextButton(
+                child: Text('OK'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Login',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 32.0),
+              TextField(
+                controller: usernameController,
+                decoration: InputDecoration(
+                  labelText: 'Phone number',
+                ),
+              ),
+              SizedBox(height: 16.0),
+              TextField(
+                controller: passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                ),
+                obscureText: true,
+              ),
+              SizedBox(height: 32.0),
+              ElevatedButton(
+                child: Text('Login'),
+                onPressed: () {
+                  login(context);
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.yellow,
+                  onPrimary: Colors.black,
+                ),
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton.icon(
+                icon: Image.asset('assets/google_logo.png',
+                    width: 20, height: 20),
+                label: Text('Sign in with Google'),
+                onPressed: () {
+                  // Perform sign in with Google logic here
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Color.fromARGB(255, 0, 0, 0),
+                  onPrimary: Colors.white,
+                ),
+              ),
+              SizedBox(height: 16.0),
+              Text('Don\'t have an account?'),
+              TextButton(
+                child: Text('Sign up'),
+                onPressed: () {
+                  // Navigate to the sign-up page here
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TrackingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tracking'),
+      ),
+      body: Center(
+        child: Text('Tracking Page'),
+      ),
+    );
+  }
+}
